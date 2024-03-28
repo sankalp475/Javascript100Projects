9983a046d08347a9642a6094e164098dbc0698c4
sankalp kumar <sankalp05oct@gmail.com>
2024-03-28 16:15:57 +0530

    removed model.js

--- QUIZ APP/model.js
+++
@@ -1,9 +0,0 @@
-
-// function modelHandler(modelElement, content) {
-   
-//    modelOpen()
-// }
-// function modelOpen(modelElement, content) {
-//     modelElement.innerHTML = "";
-//     modelElement.appendChild(content);
-// }
